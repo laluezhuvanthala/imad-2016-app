9dfa45aa248ee884178f3c74a3804769a70afb28
laluezhuvanthala <rahul.cpy@gmail.com>
2016-10-12 13:12:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one', function(req, res){
+app.get('/one', function(req, res){
     res.send('Article 1');
 });
 app.get('/ui/madi.png', function (req, res) {
